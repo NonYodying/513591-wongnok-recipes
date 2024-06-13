c8ea6594beac4b38535dc494fd52b56ed3387052
NonYodying <nonthacha.yod@pea.co.th>
2024-06-13 22:06:01 +0700

    create page and routes

--- wongnok/src/App.jsx
+++ wongnok/src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboad from './pages/Dashboard'
+import Singin from './pages/Singin'
+import Singup from './pages/Singup'
+import Recipes from './pages/Recipes'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/dashboad' element={<Dashboad />}/>
+        <Route path='/singin' element={<Singin />}/>
+        <Route path='/singup' element={<Singup />}/>
+        <Route path='/recipes' element={<Recipes />} />
+
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
