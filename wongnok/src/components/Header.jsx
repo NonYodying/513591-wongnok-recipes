06962a2c364063fe4565bb62062783e21871e775
NonYodying <nonthacha.yod@pea.co.th>
2024-06-16 18:20:51 +0700

    complete signin page functionality

--- wongnok/src/components/Header.jsx
+++ wongnok/src/components/Header.jsx
@@ -26,7 +26,7 @@ export default function Header() {
         <Button className="w-12 h-10 hidden sm:inline" color='gray' pill>
           <FaMoon />
         </Button>
-        <Link to='/singin'>
+        <Link to='/signin'>
           <Button gradientDuoTone='purpleToBlue' outline>
            Sing In
           </Button>
--- wongnok/src/pages/Signin.jsx
+++ wongnok/src/pages/Signin.jsx
@@ -1,7 +1,90 @@
-import React from 'react'
+import { TextInput, Label, Button, Alert, Spinner } from "flowbite-react"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom"
+
+
 
 export default function Singin() {
+  const [formData, setFormData] = useState({});
+  const [errorMeassage, setErrorMeassgae] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if ( !formData.email || !formData.password){
+      return setErrorMeassgae('Please fill out all fields');
+    };
+    try {
+      setLoading(true);
+      setErrorMeassgae(null);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success == false) {
+        return setErrorMeassgae(data.message);
+      }
+      setLoading(false);
+      if(res.ok) {
+        navigate('/');
+      }
+    } catch (error) {
+      setErrorMeassgae(error.message);
+      setLoading(false);
+    }
+  };
   return (
-    <div>Singin</div>
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        <div className="flex-1">
+          <Link to={"/"} className=" font-bold dark:text-white text-4xl" >
+          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white">WONGNOK</span>
+          RECIPES
+          </Link>
+          <p className="text-sm mt-5">
+            ลงชื่อเข้าใช้เลย แล้วมาแบ่งปันเมนูสุดโปรดของคุณให้ทั่วโลกได้โลกรู้จัก เพราะเราเชื่อว่าการแบ่งปันจะทำให้โลกหน้าอยู่ขึน
+          </p>
+        </div>
+
+
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+
+            <div>
+              <Label value='Email' />
+              <TextInput type="email" placeholder="somngi@company.com" id="email" onChange={handleChange}/>
+            </div>
+            <div>
+              <Label value='Password' />
+              <TextInput type="password" placeholder="********" id="password" onChange={handleChange}/>
+            </div>
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+              {
+                loading ? (
+                  <>
+                    <Spinner size='sm' />
+                    <span className="pl-3">Loading...</span>
+                  </>
+                ) : "Sign In"
+              }
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to='/signup' className="text-blue-500">Sing Up</Link>
+          </div>
+          {
+            errorMeassage && (
+              <Alert className="mt-5" color='failure'>{errorMeassage}</Alert>
+            )
+          }
+        </div>
+      </div>
+    </div>
   )
 }
