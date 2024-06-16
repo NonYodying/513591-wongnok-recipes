b9c9bc9c38caa5aa219356938f953bfed3ca05a2
NonYodying <nonthacha.yod@pea.co.th>
2024-06-16 18:02:06 +0700

    create signin API route

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import expreess from 'express';
 import { singup } from '../controllers/auth.controller.js';
+import { singin } from '../controllers/auth.controller.js';
 
 const router = expreess.Router();
 
 router.post('/signup', singup)
+router.post('/signin', singin)
 
 
 export default router;
