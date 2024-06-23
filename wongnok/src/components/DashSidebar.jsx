c2a0baa4bf43db92ac4dcee0815067e1ebdfcd8a
NonYodying <nonthacha.yod@pea.co.th>
2024-06-23 16:03:46 +0700

    Add posts section to the dashboard

--- wongnok/src/components/DashSidebar.jsx
+++ wongnok/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { Sidebar } from "flowbite-react";
-import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { HiDocumentText , HiUser,HiDocumentAdd,HiLogout  } from "react-icons/hi";
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useDispatch } from 'react-redux'
@@ -34,13 +34,23 @@ export default function DashSidebar() {
   return (
     <Sidebar className="w-full md:w-56" >
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
+        <Sidebar.ItemGroup className="'flex flex-col gap-4">
           <Link to='/dashboard?tab=profile'>
             <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark' as='div'>
                 Profile
             </Sidebar.Item>
           </Link>
-          <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout}>
+          <Link to='/dashboard?tab=posts'>
+            <Sidebar.Item active={tab === 'posts'} icon={HiDocumentText } as='div'>
+              My recipes
+            </Sidebar.Item>
+          </Link>
+          <Link to='/create-post'>
+            <Sidebar.Item icon={HiDocumentAdd} >
+                Create new recipe
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item icon={HiLogout } onClick={handleSignout}>
             Sign Out
           </Sidebar.Item>
           
